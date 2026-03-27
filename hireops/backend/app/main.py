b27667e810879577c12c08faae0571cfe30f5f7a
Samiran-1 <mondal.samiran595@gmail.com>
2026-03-27 16:48:44 +0530

    Till 4:45

--- hireops/backend/app/main.py
+++ hireops/backend/app/main.py
@@ -77,7 +77,7 @@ app = FastAPI(
 # Standard Security Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
